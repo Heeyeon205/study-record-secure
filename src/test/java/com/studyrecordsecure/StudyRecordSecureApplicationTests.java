2d845768106e33ecbe88e1ae66f19835a678111f
Lyle <procross27@gmail.com>
2025-04-08 16:29:21 +0900

    init: studyRecordSecure🎉

---
+++ src/test/java/com/studyrecordsecure/StudyRecordSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyrecordsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRecordSecureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
